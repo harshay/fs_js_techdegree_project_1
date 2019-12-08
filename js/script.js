f0d2c35376f3464d6f79cc69be582177e5b30162
harshay <harshaykr@hotmail.com>
2019-12-08 08:21:03 +0000

    array & functions updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Creator Name - Harshay Raipancholi
+Created Date - 7/12/2019
+Creator Contact - harshaykr@hotmail.com
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
@@ -14,31 +14,29 @@ project 1 - A Random Quote Generator
 var quotes = [
 
     {
-      quote : " ",
-      source : " "
+      quote : "The future belongs to those who believe in the beauty of their dreams.",
+      source : "Eleanor Roosevelt"
     },
     {
-      quote : " ",
-      source : " "
+      quote : "It is during our darkest moments that we must focus to see the light.",
+      source : "Aristotle"
     },
     {
-      quote : " ",
-      source : " ",
-      citation : " ",
-      year : " "
+      quote : " When I left you, I was but the learner. Now I am the master.",
+      source : "Darth Vader"/*,
+      citation : "Star Wars Episode IV: A New Hope",
+      year : "1977"*/
     },
     {
-      quote : " ",
-      source : " "
+      quote : "People don’t buy stock; it gets sold to them. Don’t ever forget that. ",
+      source : "Jordan Belfort"/*,
+      citation : "Wolf of Wall Street",
+      writer : "Terence Winter"*/
     },
     {
-      quote : " ",
-      source : " "
+      quote : "In the end, it's not the years in your life that count. It's the life in your years.",
+      source : "Abraham Lincoln"
     }
-
-
-
-
 
 ]
 
@@ -46,13 +44,21 @@ var quotes = [
 /***
  * `getRandomQuote` function
 ***/
+var getRandomQuote = function() {
+  var randomNumber = math.floor(math.random()*6) + 1;
+  
+  return randomNumber; 
 
+};
 
 
 /***
  * `printQuote` function
 ***/
-
+var printQuote = function() {
+  document.getElementById('quote-box').innerHTML
+   =   '<p class="quote">' +   quotes[getRandomQuote()][1]   + '';
+};
 
 
 /***
